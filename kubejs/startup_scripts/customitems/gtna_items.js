52efa8e5a5760154f83ad2db175ff7642113b4dd
Adam <7889445+DMNerd@users.noreply.github.com>
2024-04-24 22:42:40 +0200

    added rocket fins

--- kubejs/startup_scripts/customitems/gtna_items.js
+++ kubejs/startup_scripts/customitems/gtna_items.js
@@ -2,12 +2,15 @@
 StartupEvents.registry('item', e => {
     e.create('bound_stick')
 
-    e.create('reinforced_rocket_nose_cone')
-    e.create('reinforced_rocket_nose_cone_t2')
-    e.create('advanced_rocket_nose_cone')
-    e.create('advanced_rocket_nose_cone_t2')
-    e.create('advanced_rocket_nose_cone_t3')
-    
-    e.create('advanced_multisensor_rocket_nose_cone')
+    e.create('reinforced_rocket_nose_cone').displayName("Reinforced Nose Cone Tier 1")
+    e.create('reinforced_rocket_nose_cone_t2').displayName("Reinforced Nose Cone Tier 2")
+    e.create('advanced_rocket_nose_cone').displayName("Advanced Nose Cone Tier 1")
+    e.create('advanced_rocket_nose_cone_t2').displayName("Advanced Nose Cone Tier 2")
+    e.create('advanced_rocket_nose_cone_t3').displayName("Advanced Nose Cone Tier 3")
+
+    e.create('reinforced_rocket_fin').displayName("Reinforced Rocket Fin Tier 1")
+    e.create('reinforced_rocket_fin_t2').displayName("Reinforced Rocket Fin Tier 2")
+    e.create('advanced_rocket_fin').displayName("Advanced Rocket Fin")
+
     e.create('fluix_crystal_cpu')
 })
