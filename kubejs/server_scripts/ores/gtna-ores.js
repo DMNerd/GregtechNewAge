fa9035ec7031f64110ec535d91c06e17a90b976b
Adam <7889445+DMNerd@users.noreply.github.com>
2024-04-27 10:40:40 +0200

    nutrition

--- kubejs/server_scripts/ores/gtna-ores.js
+++ kubejs/server_scripts/ores/gtna-ores.js
@@ -11,10 +11,10 @@ GTCEuServerEvents.oreVeins(e => {
 
         v.layeredVeinGenerator(gen => gen
             .buildLayerPattern(pat => pat
-                .layer(l => l.weight(1).block(() => Block.getBlock('mythicbotany:dragonstone_ore').size(1, 1)))
+                //.layer(l => l.weight(1).block(() => Block.getBlock('mythicbotany:dragonstone_ore').size(1, 1)))
+                .layer(l => l.weight(1).mat(GTMaterials.get('arcane_crystal')).size(1, 1))
                 .layer(l => l.weight(2).mat(GTMaterials.Gold).size(2, 2))
                 .layer(l => l.weight(3).mat(GTMaterials.get('elementium')).size(1, 1))
-                .layer(l => l.weight(1).mat(GTMaterials.get('arcane_crystal')).size(1, 1))
             )
         )
         v.surfaceIndicatorGenerator(ind => ind 
